ad21eac287e14ee8643504ac17910862d07fb6c5
Mo Han <1063022109@qq.com>
2017-05-12 21:52:21 +0800

    新配置webapck文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+var webpack = require('webpack');
+
 module.exports = {
     //entry: ['webpack/hot/dev-server','./public/assets/js/entry.js'],
     entry: {
@@ -42,6 +44,29 @@ module.exports = {
             }
 
         ]
-    }
+    },
+    /*
+    * 将公共部分打包为vendor.index.js
+    * */
     //plugins: [ new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.index.js') ]
+
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': '"production"'
+            }
+        }),
+        //注意这里  这两个地方市用来配置common.js模块单独打包的
+        new webpack.optimize.CommonsChunkPlugin({
+            name: "vendor",//和上面配置的入口对应
+            filename: "vendor.index.js"//导出的文件的名称
+        }),
+        //压缩打包的文件
+         new webpack.optimize.UglifyJsPlugin(),
+         new webpack.optimize.OccurenceOrderPlugin(),
+         new webpack.optimize.AggressiveMergingPlugin(),
+        //允许错误不打断程序
+         new webpack.NoErrorsPlugin()
+    ]
+
 };
